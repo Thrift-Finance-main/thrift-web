6d5ce522ccbd5d3af295b860ea92163cd790686e
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-29 22:27:37 +0100

    feat: fixed svg

--- components/console/Components/World.jsx
+++ components/console/Components/World.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import Globe from './Globe.svg'
 
 function FeaturesWorld() {
   return (
@@ -19,7 +18,7 @@ function FeaturesWorld() {
           {/* World illustration */}
           <div className="flex flex-col items-center pt-12 md:pt-16">
             <div className="relative">
-                  <Image src={Globe} alt={'Team around the globe'}/>
+                  <Image src={'https://res.cloudinary.com/dhkccnvyn/image/upload/v1659129900/Globe_q8nbya.svg'} alt={'Team around the globe'} width='100%' height='100%'/>
             </div>
           </div>
 
