24483761a7be17f3ea663af43d2d63735a2f0122
jcaso <jcaso@protonmail.com>
2022-07-30 16:35:43 +0100

    fix: fleek, default loader is not compatible with `next export`

--- next.config.js
+++ next.config.js
@@ -2,17 +2,12 @@
 module.exports = {
   reactStrictMode: true,
   exportTrailingSlash: true,
-  // images: {
-  //   loader: 'akamai',
-  //   path: '/',
-  // }
-  
   images: {
-    
+
     domains: [
-      'res.cloudinary.com'
+      'res.cloudinary.com','/'
     ],
-  }
+   }
   ,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.experiments = { ...config.experiments, asyncWebAssembly: true }
