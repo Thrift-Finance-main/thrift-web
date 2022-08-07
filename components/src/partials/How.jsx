7ca63f160e22a0e97779c0ed615e6a2e0e370b14
jcaso <jcaso@protonmail.com>
2022-08-07 11:07:10 +0200

    fix:  Pink Box background

--- components/src/partials/How.jsx
+++ components/src/partials/How.jsx
@@ -24,6 +24,8 @@ function How() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [tab])
 
+  console.log('tab');
+  console.log(tab);
   return (
     <section className="relative">
 
@@ -59,42 +61,42 @@ function How() {
               {/* Tabs buttons */}
               <div className="mb-8 md:mb-0">
                 <button
-                  className={`flex other-here black-text items-center  text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${tab !== 1 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'bg-gray-200 border-transparent'}`}
+                  className={'flex black-text items-center  text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 '+ (tab === 1 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'other-here bg-gray-200 border-transparent')}
                   onClick={(e) => { e.preventDefault(); setTab(1); }}
                 >
-                  <div>
+                  <div className={" "+ (tab === 1 ? 'text-gray-900' : 'text-gray-200')}>
                     <div className="font-bold leading-snug text-left tracking-tight mb-1">Non-custodial Savings </div>
-                    <div className="text-gray-600  text-left other-here">Own your data, save your funds in the safest way, earn passively, get rewards, NFTs and much more. </div>
+                    <div className="text-left">Own your data, save your funds in the safest way, earn passively, get rewards, NFTs and much more. </div>
                   </div>
                   <div className="flex justify-center items-center w-8 h-8  bg-white rounded-full shadow shrink-0 ml-3">
-                    <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                    <svg color="gray" className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M11.953 4.29a.5.5 0 00-.454-.292H6.14L6.984.62A.5.5 0 006.12.173l-6 7a.5.5 0 00.379.825h5.359l-.844 3.38a.5.5 0 00.864.445l6-7a.5.5 0 00.075-.534z" />
                     </svg>
                   </div>
                 </button>
                 <button
-                  className={`flex other-here items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${tab !== 2 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'bg-gray-200 border-transparent'}`}
+                  className={'flex black-text items-center  text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 '+ (tab === 2 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'other-here bg-gray-200 border-transparent')}
                   onClick={(e) => { e.preventDefault(); setTab(2); }}
                 >
-                  <div >
-                    <div className="font-bold leading-snug tracking-tight mb-1 text-left">Crowd Lending</div>
-                    <div className="text-gray-600 text-left other-here">Join or create groups according to your saving goals, lend, and grow your wealth.  </div>
+                  <div className={" "+ (tab === 2 ? 'text-gray-900' : 'text-gray-200')}>
+                    <div className="font-bold leading-snug text-left tracking-tight mb-1">Crowd Lending</div>
+                    <div className="text-left">Join or create groups according to your saving goals, lend, and grow your wealth. </div>
                   </div>
-                  <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow shrink-0 ml-3">
-                    <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                  <div className="flex justify-center items-center w-8 h-8  bg-white rounded-full shadow shrink-0 ml-3">
+                    <svg color="gray" className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z" fillRule="nonzero" />
                     </svg>
                   </div>
                 </button>
                 <button
-                  className={`flex other-here items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${tab !== 3 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'bg-gray-200 border-transparent'}`}
+                  className={'flex black-text items-center  text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 '+ (tab === 3 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'other-here bg-gray-200 border-transparent')}
                   onClick={(e) => { e.preventDefault(); setTab(3); }}
                 >
-                  <div>
-                    <div className="font-bold leading-snug tracking-tight mb-1 text-left">Multiple Payments </div>
-                    <div className="text-gray-600 text-left other-here">For a single payment transaction fee, you can make multiple payments in one transaction.</div>
+                  <div className={" "+ (tab === 3 ? 'text-gray-900' : 'text-gray-200')}>
+                    <div className="font-bold leading-snug text-left tracking-tight mb-1">Multiple Payments</div>
+                    <div className="text-left">For a single payment transaction fee, you can make multiple payments in one transaction.</div>
                   </div>
-                  <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow shrink-0 ml-3">
+                  <div className="flex justify-center items-center w-8 h-8  bg-white rounded-full shadow shrink-0 ml-3">
                     <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M11.334 8.06a.5.5 0 00-.421-.237 6.023 6.023 0 01-5.905-6c0-.41.042-.82.125-1.221a.5.5 0 00-.614-.586 6 6 0 106.832 8.529.5.5 0 00-.017-.485z" fill="#191919" fillRule="nonzero" />
                     </svg>
