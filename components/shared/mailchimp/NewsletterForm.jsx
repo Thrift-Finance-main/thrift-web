c26ffe7117368dd1b42bf4a8b952aca09f0a28ee
jcaso <jcaso@protonmail.com>
2022-09-09 15:23:13 +0100

    fix: sign up message

--- components/shared/mailchimp/NewsletterForm.jsx
+++ components/shared/mailchimp/NewsletterForm.jsx
@@ -93,7 +93,7 @@ const NewsletterForm = ({ status, message, onValidated }) => {
           ) : null}
           {status === 'success' && status !== 'error' && !error && (
             // <div dangerouslySetInnerHTML={{ __html: decode(message) }} />
-            <p>thank you and confirmation text</p>
+            <p>Thanks for signing up to be notified</p>
           )}
         </div>
       </div>
